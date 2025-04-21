64249723b0824855ef2c9f0ffb24df1246e6658d
Logan Bond <lb125093@student.wpsok.org>
2025-04-21 14:03:17 -0500

    added character - masterfile made

---
+++ Character stuff.py
@@ -0,0 +1,36 @@
+#Class for players
+class Character:
+    def __init__(self, name, health, maxhealth, speed, type, origin):
+        self.name = name
+        self.health = health
+        self.maxhealth = maxhealth
+        self.speed = speed
+        self.type = type
+        self.origin = origin
+
+#class for enemy
+class Enemy:
+    def __init__(self, name, health, maxhealth, speed, type, origin):
+        self.name = name
+        self.health = health
+        self.maxhealth = maxhealth
+        self.speed = speed
+        self.type = type
+        self.origin = origin
+
+
+#Characters - L, E, J
+chopchop = Character()
+[] = Character()
+[] = Character()
+
+#Enemys - L, E, J
+[] = Enemy()
+[] = Enemy()
+[] = Enemy()
+[] = Enemy()
+[] = Enemy()
+[] = Enemy()
+[] = Enemy()
+[] = Enemy()
+[] = Enemy()
---
+++ Master File.py
@@ -0,0 +1,103 @@
+#Class for players
+class Character:
+    def __init__(self, name, health, maxhealth, speed, type, origin):
+        self.name = name
+        self.health = health
+        self.maxhealth = maxhealth
+        self.speed = speed
+        self.type = type
+        self.origin = origin
+
+#class for enemy
+class Enemy:
+    def __init__(self, name, health, maxhealth, speed, type, origin):
+        self.name = name
+        self.health = health
+        self.maxhealth = maxhealth
+        self.speed = speed
+        self.type = type
+        self.origin = origin
+
+
+#Characters - L, E, J
+chopchop = Character()
+[] = Character()
+[] = Character()
+
+#Enemys - L, E, J
+[] = Enemy()
+[] = Enemy()
+[] = Enemy()
+[] = Enemy()
+[] = Enemy()
+[] = Enemy()
+[] = Enemy()
+[] = Enemy()
+[] = Enemy()
+
+
+import random
+import time
+
+
+#character class
+class Character:
+    def __init__(self,maxhp,hp,physDamage,specDamage,defense):
+        self.maxhp = maxhp
+        self.hp = hp
+        self.physDamage = physDamage
+        self.specDamage = specDamage
+        self.defense = defense
+player = Character(20,20,6,6,10)
+#enemy class
+class Enemy:
+    def __init__(self,name,maxhp,hp,physDamage,specDamage,defense):
+        self.name = name
+        self.maxhp = maxhp
+        self.hp = hp
+        self.physDamage = physDamage
+        self.specDamage = specDamage
+        self.defense = defense
+
+currentEnemy = 0
+#list of enemies
+slime = Enemy("Slime",15,15,4,6,10)
+enemies = [slime]
+
+#combat
+#choose enemy
+def chooseEnemy():
+    global enemies
+    global currentEnemy
+    enemyRandom = random.choice(enemies)
+    currentEnemy = enemyRandom
+
+def showStats():
+    global currentEnemy
+    print("Player HP:",player.hp,"/",player.maxhp)
+    print(currentEnemy.name,"HP:",currentEnemy.hp,"/",currentEnemy.maxhp)
+
+def playerPhys():
+    global currentEnemy
+    defRoll = random.randint(1,20)
+    damageRoll = random.randint(1,player.physDamage)
+    if defRoll > currentEnemy.defense:
+        currentEnemy.hp -= damageRoll
+    else:
+        print("You")
+
+def playerSpec():
+
+def playerAction():
+    try:
+        action = int(input("(1) Strike (2) Spell:"))
+    except:
+        print("Not Valid Option")
+        playerAction()
+    if action == 1:
+        playerPhys()
+    if action == 2:
+        playerSpec
+
+chooseEnemy()
+showStats()
