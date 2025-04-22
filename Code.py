09f3dbcf3434d0769357b7019b019cd245b239d9
Jax Byrge <jb128412@student.wpsok.org>
2025-04-22 14:06:33 -0500

    Updating Combat W.I.P

--- Code.py
+++ Code.py
@@ -4,30 +4,54 @@ import time
 
 #character class
 class Character:
-    def __init__(self,maxhp,hp,physDamage,specDamage,defense):
+    def __init__(self,maxhp,hp,strength,wisdom,defense):
         self.maxhp = maxhp
         self.hp = hp
-        self.physDamage = physDamage
-        self.specDamage = specDamage
+        self.strength = strength
+        self.wisdom = wisdom
         self.defense = defense
-player = Character(20,20,6,6,10)
+
+
+#weapon class
+weapon = 0
+class Weapon:
+    def __init__(self,name,Damage,Status):
+        self.name = name
+        self.Damage = Damage
+        self.Status = Status
+
+#skill class
+skill = 0
+class Skill:
+    def __init__(self,name,Damage,Status):
+        self.name = name
+        self.Damage = Damage
+        self.Status = Status
+
+#weapon list
+sword = Weapon("Sword Slash",4,0)
+magicBolt = Skill("Magic Bolt",6,0)
 #enemy class
 class Enemy:
-    def __init__(self,name,maxhp,hp,physDamage,specDamage,defense):
+    def __init__(self,name,maxhp,hp,strength,wisdom,defense):
         self.name = name
         self.maxhp = maxhp
         self.hp = hp
-        self.physDamage = physDamage
-        self.specDamage = specDamage
+        self.strength = strength
+        self.wisdom = wisdom
         self.defense = defense
 
 currentEnemy = 0
+
 #list of enemies
 slime = Enemy("Slime",15,15,4,6,10)
 enemies = [slime]
 
 #combat
-#choose enemy
+player = Character(20,20,6,6,10)
+weapon = sword
+inCombat = True
+
 def chooseEnemy():
     global enemies
     global currentEnemy
@@ -39,38 +63,93 @@ def showStats():
     print("Player HP:",player.hp,"/",player.maxhp)
     print(currentEnemy.name,"HP:",currentEnemy.hp,"/",currentEnemy.maxhp)
 
-def playerPhys():
+def playerWeapon():
     global currentEnemy
+    global weapon
     defRoll = random.randint(1,20)
-    damageRoll = random.randint(1,player.physDamage)
+    damageRoll = random.randint(1,weapon.Damage) + player.strength
     if defRoll > currentEnemy.defense:
         currentEnemy.hp -= damageRoll
         print("You dealt ",damageRoll,"damage")
     else:
         print("You didn't hit")
+    time.sleep(1)
 
-def playerSpec():
+def playerSkill():
     global currentEnemy
     defRoll = random.randint(1,20)
-    damageRoll = random.randint(1,player.specDamage)
+    damageRoll = random.randint(1,weapon.Damage) + player.wisdom
     if defRoll > currentEnemy.defense:
         currentEnemy.hp -= damageRoll
         print("You dealt ",damageRoll,"damage")
     else:
         print("You didn't hit")
+    time.sleep(1)
 
 def playerAction():
+    global skill
+    global weapon
     try:
-        action = int(input("(1) Strike (2) Spell:"))
+        action = int(input("(1)",weapon.name,"(2)",skill.name,":"))
     except:
         print("Not Valid Option")
         playerAction()
     if action == 1:
-        playerPhys()
+        playerWeapon()
     if action == 2:
-        playerSpec()
+        playerSkill()
 
-chooseEnemy()
-showStats()
-playerAction()
-showStats()
+def enemyPhys():
+    global currentEnemy
+    defRoll = random.randint(1,20)
+    damageRoll = random.randint(1,currentEnemy.strength)
+    if defRoll > player.defense:
+        player.hp -= damageRoll
+        print(currentEnemy.name," dealt ",damageRoll,"damage")
+    else:
+        print(currentEnemy.name," didn't hit")
+    time.sleep(1)
+
+def enemySpec():
+    global currentEnemy
+    defRoll = random.randint(1,20)
+    damageRoll = random.randint(1,currentEnemy.wisdom)
+    if defRoll > player.defense:
+        player.hp -= damageRoll
+        print(currentEnemy.name," dealt ",damageRoll,"damage")
+    else:
+        print(currentEnemy.name," didn't hit")
+    time.sleep(1)
+
+def enemyAction():
+    global currentEnemy
+    pOrS = ["p","s"]
+    pSRandom = random.choice(pOrS)
+    if pSRandom == "p":
+        enemyPhys()
+    if pSRandom == "s":
+        enemySpec()
+
+def checkDeath():
+    global currentEnemy
+    global inCombat
+    if currentEnemy.hp < 1:
+        inCombat = False
+        print(currentEnemy.name,"has died")
+    if player.hp < 1:
+        inCombat = False
+        print("You have died")
+
+def goInCombat():
+    global inCombat
+    inCombat = True
+    chooseEnemy()
+    while inCombat == True:
+        showStats()
+        playerAction()
+        checkDeath()
+        enemyAction()
+        checkDeath()
+    currentEnemy = 0
+
+goInCombat()
