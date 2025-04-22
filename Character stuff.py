d27cc079c7a5b9a305b51dbe1d3a035f90b3c8a4
Logan Bond <lb125093@student.wpsok.org>
2025-04-22 14:06:45 -0500

    Updated class - added Pyro, Goblin, and Construct

--- Character stuff.py
+++ Character stuff.py
@@ -1,28 +1,32 @@
 #Class for players
 class Character:
-    def __init__(self, name, health, maxhealth, speed, type, origin):
+    def __init__(self, name, hp, maxhp, defense, physDamage, specDamage, type, origin):
         self.name = name
-        self.health = health
-        self.maxhealth = maxhealth
-        self.speed = speed
+        self.hp = hp
+        self.maxhp = maxhp
+        self.defense = defense
+        self.physDamage = physDamage
+        self.specDamage = specDamage
         self.type = type
         self.origin = origin
 
 #class for enemy
 class Enemy:
-    def __init__(self, name, health, maxhealth, speed, type, origin):
+    def __init__(self, name, hp, maxhp, defense, physDamage, specDamage, type, origin):
         self.name = name
-        self.health = health
-        self.maxhealth = maxhealth
-        self.speed = speed
+        self.hp = hp
+        self.maxhp = maxhp
+        self.defense = defense
+        self.physDamage = physDamage
+        self.specDamage = specDamage
         self.type = type
         self.origin = origin
 
 
 #Characters - L, E, J
-chopchop = Character()
-[] = Character()
-[] = Character()
+pyro = Character("Pyro", 19, 19, 9, 2, 10, "Pyromaniac Mercenary", "Team Fortress 2")
+goblin = Character("Goblin", 17, 22, 6, 1, 3, "Collector", "None")
+construct = Character("Construct", 20, 20, 12, 4, 8, "Mage Robot", "Slay the Spire")
 
 #Enemys - L, E, J
 [] = Enemy()
