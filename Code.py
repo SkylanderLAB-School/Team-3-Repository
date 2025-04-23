8be5559362d9d6d84cb95aa8098b4dd7435c9d7d
JaxByrge <jb128412@student.wpsok.org>
2025-04-23 13:22:51 -0500

    Code.py

--- Code.py
+++ Code.py
@@ -1,76 +1,187 @@
-import random
 import time
+import random
 
-
-#character class
+#Character Class
 class Character:
-    def __init__(self,maxhp,hp,physDamage,specDamage,defense):
-        self.maxhp = maxhp
-        self.hp = hp
-        self.physDamage = physDamage
-        self.specDamage = specDamage
-        self.defense = defense
-player = Character(20,20,6,6,10)
-#enemy class
-class Enemy:
-    def __init__(self,name,maxhp,hp,physDamage,specDamage,defense):
+    def __init__(self, name,hpMax,hp,strength,magic,ac):
         self.name = name
-        self.maxhp = maxhp
+        self.hpMax = hpMax
         self.hp = hp
-        self.physDamage = physDamage
-        self.specDamage = specDamage
-        self.defense = defense
+        self.strength = strength
+        self.magic = magic
+        self.ac = ac
+    def dealDamage(self, amount):
+        self.hp -= amount
 
-currentEnemy = 0
-#list of enemies
-slime = Enemy("Slime",15,15,4,6,10)
-enemies = [slime]
+#Enemy Class
+class Enemy:
+    def __init__(self, name,hpMax,hp,strength,magic,ac,magicRoll,strengthRoll,strengthStatus,magicStatus):
+        self.name = name
+        self.hpMax = hpMax
+        self.hp = hp
+        self.strength = strength
+        self.magic = magic
+        self.ac = ac
+        self.magicRoll = magicRoll
+        self.strengthRoll = strengthRoll
+        self.strengthStatus = strengthStatus
+        self.magicStatus = magicStatus
+    def dealDamage(self,amount):
+        self.hp -= amount
 
-#combat
-#choose enemy
-def chooseEnemy():
-    global enemies
-    global currentEnemy
-    enemyRandom = random.choice(enemies)
-    currentEnemy = enemyRandom
+#Weapon Class
+class Weapon:
+    def __init__(self, attackName,damageRoll,status):
+        self.attackName = attackName
+        self.damageRoll = damageRoll
+        self.status = status
 
-def showStats():
-    global currentEnemy
-    print("Player HP:",player.hp,"/",player.maxhp)
-    print(currentEnemy.name,"HP:",currentEnemy.hp,"/",currentEnemy.maxhp)
+#Skill Class
+class Skill:
+    def __init__(self, attackName,damageRoll,status):
+        self.attackName = attackName
+        self.damageRoll = damageRoll
+        self.status = status
 
-def playerPhys():
-    global currentEnemy
-    defRoll = random.randint(1,20)
-    damageRoll = random.randint(1,player.physDamage)
-    if defRoll > currentEnemy.defense:
-        currentEnemy.hp -= damageRoll
-        print("You dealt ",damageRoll,"damage")
+#Variables
+currentEnemy = None
+currentCharacter = None
+heldWeapon = None
+skill = None
+inCombat = False
+
+#Characters
+construct = Character("Construct",70,70,3,5,12)
+
+#Enemies
+owlBear = Enemy("Owl Bear",120,120,6,2,10,10,25,None,None)
+enemyList = [owlBear]
+
+#Weapons
+sword = Weapon("Slash",15,None)
+
+#Skills
+burn = Skill("Burn",10,"burn")
+
+#Combat
+def chooseCharacter():
+    global currentCharacter
+    global skill
+    global heldWeapon
+    heldWeapon = sword
+    characterChoice = int(input("Choose a character (1) Construct: "))
+    if characterChoice == 1:
+        currentCharacter = construct
+        skill = burn
     else:
-        print("You didn't hit")
+        chooseCharacter()
 
-def playerSpec():
+def showFightStats():
+    global currentCharacter
     global currentEnemy
-    defRoll = random.randint(1,20)
-    damageRoll = random.randint(1,player.specDamage)
-    if defRoll > currentEnemy.defense:
-        currentEnemy.hp -= damageRoll
-        print("You dealt ",damageRoll,"damage")
-    else:
-        print("You didn't hit")
+    print("Your hp: ",currentCharacter.hp,"/",currentCharacter.hpMax)
+    print(currentEnemy.name, "hp: ", currentEnemy.hp, "/", currentEnemy.hpMax)
 
+def checkDeath():
+    global currentCharacter
+    global currentCharacter
+    if currentEnemy.hp < 1:
+        inCombat = False
+        print("You killed ",currentEnemy.name)
+    if currentCharacter.hp < 1:
+        print("You have died")
+        exit(0)
+
+#player actions
 def playerAction():
-    try:
-        action = int(input("(1) Strike (2) Spell:"))
-    except:
-        print("Not Valid Option")
+    global currentCharacter
+    global heldWeapon
+    global skill
+    print("(1) ",heldWeapon.attackName)
+    print("(2) ",skill.attackName)
+    action = input("Choose an action: ")
+    if action == "1":
+        playerWeaponAttack()
+    elif action == "2":
+        playerMagicAttack()
+    else:
         playerAction()
-    if action == 1:
-        playerPhys()
-    if action == 2:
-        playerSpec()
 
-chooseEnemy()
-showStats()
-playerAction()
-showStats()
+def playerWeaponAttack():
+    global currentCharacter
+    global heldWeapon
+    global currentEnemy
+    dodgeRoll = random.randint(1,20)
+    if dodgeRoll > currentEnemy.ac:
+        damage = random.randint(1,heldWeapon.damageRoll) + currentCharacter.strength
+        currentEnemy.dealDamage(damage)
+        print("You deal ", damage, "damage")
+    else:
+        print("You miss")
+    time.sleep(1)
+
+def playerMagicAttack():
+    global currentCharacter
+    global skill
+    global currentEnemy
+    dodgeRoll = random.randint(1,20) + currentCharacter.magic
+    if dodgeRoll > currentEnemy.ac:
+        damage = random.randint(1,skill.damageRoll)
+        currentEnemy.dealDamage(damage)
+        print("You deal ", damage, "damage")
+    else:
+        print("You miss")
+    time.sleep(1)
+
+#enemy actions
+def enemyActions():
+    global currentEnemy
+    global currentCharacter
+    sOrM = random.randint(1,2)
+    if sOrM == 1:
+        enemyWeaponAttack()
+    if sOrM == 2:
+        enemyMagicAttack()
+
+def enemyWeaponAttack():
+    global currentCharacter
+    global currentEnemy
+    dodgeRoll = random.randint(1,20)
+    if dodgeRoll > currentCharacter.ac:
+        damage = random.randint(1,currentEnemy.strengthRoll) + currentEnemy.strength
+        currentCharacter.dealDamage(damage)
+        print(currentEnemy.name, " dealt ", damage, "damage")
+    else:
+        print(currentEnemy.name," missed")
+    time.sleep(1)
+
+def enemyMagicAttack():
+    global currentCharacter
+    global currentEnemy
+    dodgeRoll = random.randint(1,20) + currentEnemy.magic
+    if dodgeRoll > currentCharacter.ac:
+        damage = random.randint(1,currentEnemy.magicRoll)
+        currentCharacter.dealDamage(damage)
+        print(currentEnemy.name," dealt ", damage, "damage")
+    else:
+        print(currentEnemy.name," missed")
+    time.sleep(1)
+
+#combat loop
+def startCombat(enemyChoice):
+    global currentCharacter
+    global currentEnemy
+    inCombat = True
+    currentEnemy = enemyChoice
+    print("You have encountered a ",currentEnemy.name)
+    time.sleep(1)
+    while inCombat == True:
+        showFightStats()
+        time.sleep(1)
+        playerAction()
+        enemyActions()
+        checkDeath()
+    currentEnemy = 0
+
+chooseCharacter()
+startCombat(owlBear)
