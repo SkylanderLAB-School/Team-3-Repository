911a3f5b0f5d19cda23322873fd96e84c858f32a
Jax Byrge <jb128412@student.wpsok.org>
2025-04-23 14:05:02 -0500

    Finished combat added status effects into the combat

--- Code.py
+++ Code.py
@@ -1,5 +1,6 @@
 import random
 import time
+from asyncio import current_task
 
 
 #character class
@@ -33,19 +34,40 @@ import random
 
 #Character Class
 class Character:
-    def __init__(self, name,hpMax,hp,strength,magic,ac):
+    def __init__(self, name,hpMax,hp,strength,magic,ac,currentStatus,statusLevel):
         self.name = name
         self.hpMax = hpMax
         self.hp = hp
         self.strength = strength
         self.magic = magic
         self.ac = ac
+        self.currentStatus = currentStatus
+        self.statusLevel = statusLevel
     def dealDamage(self, amount):
         self.hp -= amount
+    def burn(self):
+        self.hp -= self.statusLevel
+        print("You were burned for ", self.statusLevel, "damage")
+        self.statusLevel -= self.strength
+        if self.statusLevel < 1:
+            print("You no longer burned")
+            self.statusLevel = 0
+            self.currentStatus = None
+        time.sleep(1)
+    def poison(self):
+        self.hp -= self.statusLevel
+        print("You were poisoned for ", self.statusLevel, "damage")
+        self.statusLevel -= self.magic
+        if self.statusLevel < 1:
+            print("You no longer poisoned")
+            self.statusLevel = 0
+            self.currentStatus = None
+        time.sleep(1)
+
 
 #Enemy Class
 class Enemy:
-    def __init__(self, name,hpMax,hp,strength,magic,ac,magicRoll,strengthRoll,strengthStatus,magicStatus):
+    def __init__(self, name,hpMax,hp,strength,magic,ac,magicRoll,strengthRoll,strengthStatus,magicStatus,currentStatus,statusLevel):
         self.name = name
         self.hpMax = hpMax
         self.hp = hp
@@ -56,8 +78,28 @@ class Enemy:
         self.strengthRoll = strengthRoll
         self.strengthStatus = strengthStatus
         self.magicStatus = magicStatus
+        self.currentStatus = currentStatus
+        self.statusLevel = statusLevel
     def dealDamage(self,amount):
         self.hp -= amount
+    def burn(self):
+        self.hp -= self.statusLevel
+        print(self.name, "was burned for ", self.statusLevel, "damage")
+        self.statusLevel -= self.strength
+        if self.statusLevel < 1:
+            print(self.name,"is no longer burned")
+            self.statusLevel = 0
+            self.currentStatus = None
+        time.sleep(1)
+    def poison(self):
+        self.hp -= self.statusLevel
+        print(self.name, "was poisoned for ", self.statusLevel, "damage")
+        self.statusLevel -= self.magic
+        if self.statusLevel < 1:
+            print(self.name,"is no longer poisoned")
+            self.statusLevel = 0
+            self.currentStatus = None
+        time.sleep(1)
 
 #Weapon Class
 class Weapon:
@@ -81,17 +123,17 @@ skill = None
 inCombat = False
 
 #Characters
-construct = Character("Construct",70,70,3,5,12)
+construct = Character("Construct",70,70,3,5,12,None,None)
 
 #Enemies
-owlBear = Enemy("Owl Bear",120,120,6,2,10,10,25,None,None)
+owlBear = Enemy("Owl Bear",120,120,6,2,10,10,25,None,"poison",None,None)
 enemyList = [owlBear]
 
 #Weapons
 sword = Weapon("Slash",15,None)
 
 #Skills
-burn = Skill("Burn",10,"burn")
+overheat = Skill("Overheat",10,"burn")
 
 #Combat
 def chooseCharacter():
@@ -102,7 +144,7 @@ def chooseCharacter():
     characterChoice = int(input("Choose a character (1) Construct: "))
     if characterChoice == 1:
         currentCharacter = construct
-        skill = burn
+        skill = overheat
     else:
         chooseCharacter()
 
@@ -146,6 +188,16 @@ def playerWeaponAttack():
         damage = random.randint(1,heldWeapon.damageRoll) + currentCharacter.strength
         currentEnemy.dealDamage(damage)
         print("You deal ", damage, "damage")
+        if heldWeapon.status == "burn":
+            currentEnemy.currentStatus = "burn"
+            currentEnemy.statusLevel = 10
+            print("You inflict Burn")
+            time.sleep(1)
+        if heldWeapon.status == "poison":
+            currentEnemy.currentStatus = "poison"
+            currentEnemy.statusLevel = 10
+            print("You inflict Poison")
+            time.sleep(1)
     else:
         print("You miss")
     time.sleep(1)
@@ -159,6 +211,16 @@ def playerMagicAttack():
         damage = random.randint(1,skill.damageRoll)
         currentEnemy.dealDamage(damage)
         print("You deal ", damage, "damage")
+        if skill.status == "burn":
+            currentEnemy.currentStatus = "burn"
+            currentEnemy.statusLevel = 10
+            print("You inflict Burn")
+            time.sleep(1)
+        if skill.status == "poison":
+            currentEnemy.currentStatus = "poison"
+            currentEnemy.statusLevel = 10
+            print("You inflict Poison")
+            time.sleep(1)
     else:
         print("You miss")
     time.sleep(1)
@@ -181,6 +243,16 @@ def enemyWeaponAttack():
         damage = random.randint(1,currentEnemy.strengthRoll) + currentEnemy.strength
         currentCharacter.dealDamage(damage)
         print(currentEnemy.name, " dealt ", damage, "damage")
+        if currentEnemy.strenthStatus == "burn":
+            currentCharacter.currentStatus = "burn"
+            currentCharacter.statusLevel = 10
+            print(currentEnemy.name," inflicts Burn")
+            time.sleep(1)
+        if currentEnemy.strenthStatus == "poison":
+            currentCharacter.currentStatus = "poison"
+            currentCharacter.statusLevel = 10
+            print(currentEnemy.name," inflicts Poison")
+            time.sleep(1)
     else:
         print(currentEnemy.name," missed")
     time.sleep(1)
@@ -193,6 +265,16 @@ def enemyMagicAttack():
         damage = random.randint(1,currentEnemy.magicRoll)
         currentCharacter.dealDamage(damage)
         print(currentEnemy.name," dealt ", damage, "damage")
+        if currentEnemy.magicStatus == "burn":
+            currentCharacter.currentStatus = "burn"
+            currentCharacter.statusLevel = 10
+            print(currentEnemy.name," inflicts Burn")
+            time.sleep(1)
+        if currentEnemy.magicStatus == "poison":
+            currentCharacter.currentStatus = "poison"
+            currentCharacter.statusLevel = 10
+            print(currentEnemy.name," inflicts Poison")
+            time.sleep(1)
     else:
         print(currentEnemy.name," missed")
     time.sleep(1)
@@ -209,9 +291,30 @@ def startCombat(enemyChoice):
         showFightStats()
         time.sleep(1)
         playerAction()
+        checkStatusPlayer()
         enemyActions()
+        checkStatusEnemy()
         checkDeath()
     currentEnemy = 0
+
+#status check
+def checkStatusPlayer():
+    global currentCharacter
+    global currentEnemy
+    if currentCharacter.currentStatus != None:
+        if currentCharacter.currentStatus == "burn":
+            currentCharacter.burn()
+        if currentCharacter.currentStatus == "poison":
+            currentCharacter.poison()
+
+def checkStatusEnemy():
+    global currentCharacter
+    global currentEnemy
+    if currentEnemy.currentStatus != None:
+        if currentEnemy.currentStatus == "burn":
+            currentEnemy.burn()
+        if currentEnemy.currentStatus == "poison":
+            currentEnemy.poison()
 
 chooseCharacter()
 startCombat(owlBear)
